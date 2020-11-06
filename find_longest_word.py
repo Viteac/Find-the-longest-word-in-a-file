48e833e6f9581cb4c193ef89511ea7c574de668a
Zigiziggi <32931324+Zigiziggi@users.noreply.github.com>
2020-11-06 16:34:20 +0000

    Create find_longest_word.py

---
+++ find_longest_word.py
@@ -0,0 +1,22 @@
+# Seeking for the longest word in a Dictionary
+
+from collections import Counter
+import time
+
+words = open('english').read().splitlines()
+
+time_before = time.time()
+
+k = Counter(words)
+
+longest = max(k, key=len)
+print('The longest word in Dictionary is:', longest, 'has' , len(longest), 'characters')
+
+time_after = time.time()
+time_taken = time_after - time_before
+print ( 'Longest word found in: ' , time_taken)
+print(".......................")
+
+
+
+
